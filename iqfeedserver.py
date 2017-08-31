35a230d9b7f1dfda14cf1440a69e46e35cb25fdd
Yonatan Geifman <yonatangeifman@123.local>
2017-09-01 00:36:16 +0300

    first commit

---
+++ iqfeedserver.py
@@ -0,0 +1,61 @@
+buys=0
+sells=0
+
+bestbid = 0
+bestask = 0
+
+
+import socket
+import datetime
+
+TCP_IP = '127.0.0.1'
+TCP_PORT_IN = 5009
+TCP_PORT_OUT = 30000
+
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sout = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sout.bind((TCP_IP, TCP_PORT_OUT))
+sout.listen(1)
+connout, addr = sout.accept()
+print ('Connection address:', addr)
+
+s.connect((TCP_IP, TCP_PORT_IN))
+now = datetime.datetime.now()
+s.send(bytes('t@NQZ16\n','ascii'))
+
+s.send(bytes('w@NQZ16\n','ascii'))
+incomplete = ''
+while 1:
+    datafull = (s.recv(BUFFER_SIZE)).decode('utf-8')
+    datafull = str(incomplete+datafull).split('\n')
+    incomplete = datafull.pop()
+
+    for data in datafull:
+        data = str(data)
+        #print(data)
+        if len(data)>0:
+            if data[0]=='Q':
+                data = data.split(',')
+                if data[17][-1]=='t':
+                    # get trade update last and quantity (volume)
+                    last = float(data[3])
+                    quant = float(data[7])
+                    if last==bestbid:
+                        sells = sells+quant
+                    if last==bestask:
+                        buys = buys+quant
+                bestbid = float(data[10])
+                bestask = float(data[11])
+                message =bytes(str(bestbid)+','+str(bestask)+','+str(buys)+','+str(sells)+'\n','ascii')
+                connout.send(message)
+            if data[0]=='T':
+                #time stamp, print it
+                print(data)
+    now = datetime.datetime.now()
+
+
+
+
